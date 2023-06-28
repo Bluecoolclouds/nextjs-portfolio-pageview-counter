0895399c3b8e29164eddf047b79c138aee3a3118
Bluecoolclouds <49954845+Bluecoolclouds@users.noreply.github.com>
2023-06-28 21:08:44 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,19 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
 	title: {
-		default: "chronark.com",
-		template: "%s | chronark.com",
+		default: "HDMGroup.com",
+		template: "%s | HDMGroup.com",
 	},
 	description: "Software engineer at upstash.com and founder of planetfall.io",
 	openGraph: {
-		title: "chronark.com",
+		title: "HDMGroup.com",
 		description:
 			"Software engineer at upstash.com and founder of planetfall.io",
-		url: "https://chronark.com",
-		siteName: "chronark.com",
+		url: "https://HDMGroup.com",
+		siteName: "HDMGroup.com",
 		images: [
 			{
-				url: "https://chronark.com/og.png",
+				url: "https://HDMGroup.com/og.png",
 				width: 1920,
 				height: 1080,
 			},
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
 		},
 	},
 	twitter: {
-		title: "Chronark",
+		title: "HDMGroup",
 		card: "summary_large_image",
 	},
 	icons: {
