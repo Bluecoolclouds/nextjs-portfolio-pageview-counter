828e8b25fb5520a6a2999827ba9162b140b77cda
Bluecoolclouds <49954845+Bluecoolclouds@users.noreply.github.com>
2023-06-29 16:35:46 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import React from "react";
 import Particles from "./components/particles";
 
 const navigation = [
-	{ name: "Проекты", href: "/projects" },
-	{ name: "Контакты", href: "/contact" },
+	{ name: "Projects", href: "/projects" },
+	{ name: "Contact", href: "/contact" },
 ];
 
 export default function Home() {
@@ -29,23 +29,24 @@ export default function Home() {
 				quantity={100}
 			/>
 			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-				HDM Group
+				HDM group
 			</h1>
+
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-					Привет, здесь я держу свои проекты
-					Мой tg{" "}
+					Привет, my name is Andreas, I'm building serverless and open source
+					solutions at{" "}
 					<Link
 						target="_blank"
 						href="https://t.me/Aqqtqqt"
 						className="underline duration-500 hover:text-zinc-300"
 					>
-						Telegram
+						Upstash
 					</Link>
 
 					<br />
-					работаю сейчас над{" "}
+					and working on{" "}
 					<Link
 						target="_blank"
 						href="https://planetfall.io"
