42fba95dc473df56c6c4cc84ef298a877272495b
Bluecoolclouds <49954845+Bluecoolclouds@users.noreply.github.com>
2023-07-01 00:00:40 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,11 @@ export default function Home() {
 			<h1 className="z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
 				HDM Group
 			</h1>
-
+			<button
+      className="rounded-full px-4 py-2 bg-black text-white hover:bg-gray-700 transition-colors"
+    >
+      ЛОЛ
+    			</button>
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
@@ -51,15 +55,3 @@ export default function Home() {
 		</div>
 	);
 }
-const Button = () => {
-  const buttonStyle = {
-    padding: "10px 20px",
-    border: "2px solid",
-    borderRadius: "20px",
-    backgroundColor: "transparent",
-    color: "#000000",
-    cursor: "pointer",
-  };
-
-  return <button style={buttonStyle}>СММ</button>;
-};
