26402a484b3b8b2629f5bd543f58a75e1f8d2d75
Bluecoolclouds <49954845+Bluecoolclouds@users.noreply.github.com>
2023-12-07 02:31:44 +0400

    Update page.tsx

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -14,12 +14,7 @@ type Props = {
 	};
 };
 
-const redis = new Redis({
-	url: 'https://intense-whippet-48589.upstash.io',
-	token: 'Ab3NACQgYmJiNjRjNDYtYzljMC00ODY2LTljMzYtM2E0YmU2NzZlZjVjYTk3NjMzYTY0OTM1NDEzY2I5MDhkYjBhNmMzNTUyMTU=',
-})
-
-const data = await redis.set('foo', 'bar');
+const redis = Redis.fromEnv();
 
 
 export async function generateStaticParams(): Promise<Props["params"][]> {
