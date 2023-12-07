47534e0759cd75a1104a1f4b8ded550f021f5a61
Bluecoolclouds <49954845+Bluecoolclouds@users.noreply.github.com>
2023-12-07 16:47:08 +0400

    Update page.tsx

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -72,7 +72,7 @@ export default async function ProjectsPage() {
 									<span className="flex items-center gap-1 text-xs text-zinc-500">
 										<Eye className="w-4 h-4" />{" "}
 										{Intl.NumberFormat("en-US", { notation: "compact" }).format(
-											views[featured.slug] ?? + 280 0,
+											views[featured.slug] + 280 ?? 0,
 										)}
 									</span>
 								</div>
